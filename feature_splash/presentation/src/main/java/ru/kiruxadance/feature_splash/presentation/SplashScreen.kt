353015bc710b1459a2c89a43b4ee96cc32a9380e
kiruxadance <brl199988@gmail.com>
2023-07-15 23:40:20 +0300

    screen-1: add splash screen

--- feature_splash/presentation/src/main/java/ru/kiruxadance/feature_splash/presentation/SplashScreen.kt
+++ feature_splash/presentation/src/main/java/ru/kiruxadance/feature_splash/presentation/SplashScreen.kt
@@ -1,16 +1,21 @@
 package ru.kiruxadance.feature_splash.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import ru.kiruxadance.feature_splash.presentation.ui.theme.MangoAppTheme
+import ru.kiruxadance.feature_splash.presentation.ui.theme.Typography
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,8 +26,10 @@ fun SplashScreen(
     val scaffoldState = rememberScrollState()
     
     Scaffold() { contentPadding ->
-        Column(modifier = Modifier.padding(contentPadding)) {
-            Text(text = "Hello Splash")
+        Column(modifier = Modifier.padding(contentPadding).fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "mango.", style = Typography.titleLarge)
         }
     }
 }
@@ -33,8 +40,10 @@ fun SplashScreen() {
     val scaffoldState = rememberScrollState()
 
     Scaffold() { contentPadding ->
-        Column(modifier = Modifier.padding(contentPadding)) {
-            Text(text = "Hello Splash")
+        Column(modifier = Modifier.padding(contentPadding).fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "mango.", style = Typography.titleLarge)
         }
     }
 }
